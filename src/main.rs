55e9039674c6e85d3e83f571becf91cac78667f4
Carter Snook <cartersnook04@gmail.com>
2021-02-14 08:41:44 -0600

    chore: remove useless route

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,11 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 mod chat;
 mod translate;
 
-async fn greet(_: HttpRequest) -> impl Responder {
-  String::from("Hello, world!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
       .wrap(NormalizePath::new(TrailingSlash::Trim))
-      .route("/", web::get().to(greet))
       .route("/chat", web::get().to(chat::index))
       .route("/translate", web::get().to(translate::index))
   })
